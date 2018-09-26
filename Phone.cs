0707696519ca31af16c009d80f4747159c9efb4e
clpatel80 <39173646+clpatel80@users.noreply.github.com>
2018-09-26 18:33:16 -0400

    Add files via upload

---
+++ Phone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunPhone_3
+{
+    public class Phone
+    {
+        public string Brand { }
+        public string Model { }
+        public int Storage { }
+        public double Screensize { }
+        public void { }
+        {
+        }
+    }
+        
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunPhone_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone Phone = new Phone(Enter);
+            Console.WriteLine("");
+            phone.Brand = Console.ReadLine();
+        }
+    }
+}
